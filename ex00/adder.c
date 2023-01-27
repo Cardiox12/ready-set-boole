9cea4fa6e873a14b0585178cde8f45c41e878ee6
Tony <bbellavi@student.42.fr>
2023-01-27 13:36:34 +0100

    Add: GET_CARRY and GET_RESULT macros to retrieve carry and result from encoded byte

--- ex00/adder.c
+++ ex00/adder.c
@@ -6,11 +6,11 @@
 // Result and Carry is encoded in 2 bits unsigned char : |   0   |   0   |   0   |   0   |   0   |   0   |   0   |   0   |
 
 
-byte_t half_adder(byte_t a, byte_t b) {
+static inline byte_t half_adder(byte_t a, byte_t b) {
     return MAKE_RESULT(a ^ b, a & b);
 }
 
-byte_t full_adder(byte_t a, byte_t b, byte_t cin) {
+static inline byte_t full_adder(byte_t a, byte_t b, byte_t cin) {
     byte_t ab_xor = a ^ b;
 
     return MAKE_RESULT(ab_xor ^ cin, (ab_xor & cin) | (a & b));
@@ -29,9 +29,11 @@ int     main(){
     };
 
     for (size_t i = 0 ; i < 4 ; ++i){
-        print8(half_adder(bytes[i][0], bytes[i][1]));
+        byte_t result = half_adder(bytes[i][0], bytes[i][1]);
+
+        printf("Sum: %i - Carry: %i\n", GET_RESULT(result), GET_CARRY(result));
     }
-    puts("\n\n");
+    puts("");
     int full_adder_bytes[8][3] = {
         {0, 0, 0},
         {0, 1, 0},
@@ -44,11 +46,13 @@ int     main(){
     };
 
     for (size_t i = 0 ; i < 8 ; ++i){
-        print8(full_adder(
+        byte_t result = full_adder(
             full_adder_bytes[i][0],
             full_adder_bytes[i][1],
             full_adder_bytes[i][2]
-        ));
+        );
+        printf("Sum: %i - Carry: %i\n", GET_RESULT(result), GET_CARRY(result));
+        // print8(result);
     }
     return 0;
 }
