c1095b93af794c150ae914522cf8d073501ff3a4
Tony <bbellavi@student.42.fr>
2023-01-27 13:27:26 +0100

    Add: full adder

--- ex00/adder.c
+++ ex00/adder.c
@@ -5,14 +5,16 @@
 //                                                       |  Res  | Carry |.....................EMPTY.....................| 
 // Result and Carry is encoded in 2 bits unsigned char : |   0   |   0   |   0   |   0   |   0   |   0   |   0   |   0   |
 
-byte_t half_adder(uint32_t a, uint32_t b) {
+
+byte_t half_adder(byte_t a, byte_t b) {
     return MAKE_RESULT(a ^ b, a & b);
 }
 
-// Encode Sum and Carry
-// int64_t full_adder(int32_t a, int32_t b) {
-//     int32_t sum = ()
-// }
+byte_t full_adder(byte_t a, byte_t b, byte_t cin) {
+    byte_t ab_xor = a ^ b;
+
+    return MAKE_RESULT(ab_xor ^ cin, (ab_xor & cin) | (a & b));
+}
 
 // int32_t adder(int32_t a, int32_t b) {
     
@@ -28,7 +30,25 @@ int     main(){
 
     for (size_t i = 0 ; i < 4 ; ++i){
         print8(half_adder(bytes[i][0], bytes[i][1]));
-        puts("");
+    }
+    puts("\n\n");
+    int full_adder_bytes[8][3] = {
+        {0, 0, 0},
+        {0, 1, 0},
+        {1, 0, 0},
+        {1, 1, 0},
+        {0, 0, 1},
+        {0, 1, 1},
+        {1, 0, 1},
+        {1, 1, 1},
+    };
+
+    for (size_t i = 0 ; i < 8 ; ++i){
+        print8(full_adder(
+            full_adder_bytes[i][0],
+            full_adder_bytes[i][1],
+            full_adder_bytes[i][2]
+        ));
     }
     return 0;
 }
